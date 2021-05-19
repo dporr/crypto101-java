1db67500dd54f6940c6239b7807d59912ae17d06
Diego Porras (daporr) <diego.porras@owasp.org>
2021-05-19 01:02:40 -0600

    PBKDF2 implementation test

--- src/main/java/HashingPasswords.java
+++ src/main/java/HashingPasswords.java
@@ -1,9 +1,13 @@
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
 import java.util.Base64;
 
 public class HashingPasswords {
+    private static final int ITERATIONS = 1000; //As per NIST recomendations https://eprint.iacr.org/2019/161.pdf
+    private static final int SALT_SIZE = 16; //16 bits salt as per NIST recomendation
+    private static final int HASH_LENGHT = 256; //https://cryptobook.nakov.com/mac-and-key-derivation/pbkdf2
     /*Steps for using PBDKF2:
     * 1.Password should not be empty
     * 2. Instantiate a SecretKeyFactory, java supports:
@@ -14,13 +18,40 @@ public class HashingPasswords {
     *3. Generate a SecretKey Object
     * 4. Convert the secretKey bytes to a b64 encoded string
     * */
-    public String pbdkf2(char[] password, byte[] salt, int iterations, int keylenght ) throws Exception{
+    public static String pbdkf2(char[] password, byte[] salt, int iterations, int keylenght ) throws Exception{
         if (password.length == 0)
             throw new IllegalArgumentException("Password cannot be empty");
         SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHMACSHA256");
         SecretKey key = skf.generateSecret(
                 new PBEKeySpec(password, salt, iterations, keylenght)
         );
-        return Base64.getEncoder().encodeToString(key.getEncoded());
+        StringBuilder sb = new StringBuilder();
+        sb.append(Base64.getEncoder().encodeToString(salt));
+        sb.append(":");
+        sb.append(ITERATIONS);
+        sb.append(":");
+        sb.append(Base64.getEncoder().encodeToString(key.getEncoded()));
+        return sb.toString();
+    }
+
+    public static void main(String args[]){
+        //Generate a secure salt
+        byte[] salt = new byte[SALT_SIZE];
+        SecureRandom secureRandom = new SecureRandom();
+
+        String pass1 = "password123";
+        String pass2 = "admin";
+        String hash1 = "";
+        String hash2 = "";
+        try {
+            secureRandom.nextBytes(salt);
+            hash1 = pbdkf2(pass1.toCharArray(), salt, ITERATIONS, HASH_LENGHT);
+            secureRandom.nextBytes(salt);
+            hash2 = pbdkf2(pass2.toCharArray(), salt, ITERATIONS, HASH_LENGHT);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(hash1);
+        System.out.println(hash2);
     }
 }
